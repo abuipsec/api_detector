bad5b7dfda9140b1b471c3988f72ee7aa7ea9400
Abu Faizal <42908519+abuipsec@users.noreply.github.com>
2024-01-09 00:55:49 +0300

    Update __init__.py

---
+++ api_detector/__init__.py
@@ -0,0 +1,15 @@
+# api_detector/__init__.py
+
+# This file can remain empty or include package-level initialization code.
+# It's often used to turn a directory into a Python package.
+# If your package requires initialization, you can include code here.
+
+# For example, if you want to import specific modules when the package is imported,
+# you can add those imports here:
+
+# from . import api_detector
+# from . import network_traffic_analysis
+# from . import url_patterns
+# from . import http_methods
+# from . import user_interface
+# from . import logging_utils
