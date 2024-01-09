6ac7e2d327f06d3c20701d1b6277b5ac22a27b95
Abu Faizal <42908519+abuipsec@users.noreply.github.com>
2024-01-09 21:38:49 +0300

    comit

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+    # api_detector/tests/__init__.py
+
+# This file can remain empty. Its presence makes the 'tests' directory a Python package.
+# You may add initialization code here if needed in the future.
